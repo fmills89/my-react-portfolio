a4dd1ce58330b43d02b81c6916d44ba7703df3fd
FoboIX <89666151+fmills89@users.noreply.github.com>
2022-03-25 22:41:41 -0500

    about me updated

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -3,14 +3,20 @@ import React from 'react';
 
 function About() {
     return (
-        <section className="my-5">
-            <h1 id="about">Who am I?</h1>
-            <div className="my-2">
-                    <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec bibendum turpis sed ex condimentum molestie. Mauris condimentum lectus ut ornare dignissim. Mauris faucibus urna mi, ac feugiat metus aliquam maximus. Proin aliquam justo nec diam vulputate vestibulum. Aenean sollicitudin nulla at nisi ornare, nec suscipit massa eleifend. Morbi tristique justo vel turpis sollicitudin, et tristique velit convallis. In hac habitasse platea dictumst. Phasellus mattis nunc sed orci consequat laoreet. Praesent id nisl nibh. Curabitur imperdiet ultricies mollis. In hac habitasse platea dictumst.
-                    </p>
-            </div>
-        </section>
+       <div>
+           <p className='content is-medium'>Full Stack Developer</p>
+           <hr />
+           <img className="my-pic" src={require('../../assets/pics/89666151.jpg')} alt="Forrest Mills"/>
+           <p className='content mt-4'>
+           👋 Hello world! My name is Forrest and I am a 
+            full stack developer or something close to it!
+            I recently graduated from a Trilogy Bootcamp offered through UTSA's School of Data Science.
+           </p>
+           <p className='content'>
+               Over the course of 6 months I have learned 
+               the stack from front-end all the way to the back end.
+           </p>
+       </div>
     );
 }
 
