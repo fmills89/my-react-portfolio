799804f86e7e9d30b27036398b0b596dc139c746
FoboIX <89666151+fmills89@users.noreply.github.com>
2022-03-28 00:15:48 -0500

    update about

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -7,7 +7,7 @@ function About() {
            <img className="my-pic" src={require('../../assets/pics/89666151.jpg')} alt="Forrest Mills"/>
            <p className='content is-medium title is-5 p-4'>About Me</p>
            <p className='content mt-4 subtitle is-5'>
-           👋 Hello world! My name is Forrest and I am a 
+           👋  My name is Forrest and I am a 
             full stack developer or something close to it!
             I recently graduated from a Trilogy Bootcamp offered through UTSA's School of Data Science.
            </p>
